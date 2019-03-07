cbbdb12e8407770fd080a7ec2558fb63e448ca51
Jeannette <jeannetteshui.ng@gmail.com>
2019-03-07 15:35:41 -0500

    add display page on memeDetails

--- src/components/meme-details/MemeDetails.js
+++ src/components/meme-details/MemeDetails.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-
+import database from '../firebase/firebase';
 class MemeDetails extends Component {
+    constructor(){
+        super();
+        this.state={
+            memes:null
+        }
+    }
+    componentDidMount(){
+        database.ref(`memes/${this.props.match.params.memeId}`).once('value',(response)=>{ 
+            this.setState({memes: response.val()})
+         })
+    }
+    componentDidUpdate(){
+        console.log('meme',this.state.meme)
+    }
     render() {
+        if(!this.state.memes)return (<div></div>)
         return (
-            <p>Placeholder for MemeDetails</p>
+            <div>
+             <p>{this.state.memes.inputOne}</p>
+            <img src={this.state.memes.images.original.url} alt={this.state.memes.title} />
+             <p>{this.state.memes.inputTwo}</p>
+            </div>
         )
     }
 }
