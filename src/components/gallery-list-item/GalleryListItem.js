8cb7e395a0a40609f478e7cac0fa2a9c9ed2068a
Jonathan <jon.wong.music@gmail.com>
2019-03-09 18:31:13 -0500

    Made gifs accessible

--- src/components/gallery-list-item/GalleryListItem.js
+++ src/components/gallery-list-item/GalleryListItem.js
@@ -19,13 +19,22 @@ class GalleryListItem extends Component {
     this.setState({ hover: false });
   };
 
+  onKeyPress = (e) => {
+    if(e.key ==='Enter') {
+      this.props.handleGalleryItem(this.props.item);
+      this.props.handleToggleModal();
+    }
+  }
+
   render() {
     const { images, title } = this.props.item;
     return (
       <li
         onClick={this.onItemClick}
+        onKeyPress={this.onKeyPress}
         onMouseEnter={this.onMouseEnterHandler}
         onMouseLeave={this.onMouseLeaveHandler}
+        tabIndex='0'
       >
         {this.state.hover && <div className="overlay" />}
         <img src={images.fixed_height.url} alt={title} />
