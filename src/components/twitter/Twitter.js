7d353ca86e77d2adb433d633759cc47e18dcb270
Mohammed <moe.abbas@gmail.com>
2019-03-12 16:59:07 -0400

    almost done meme list styling

--- src/components/twitter/Twitter.js
+++ src/components/twitter/Twitter.js
@@ -5,7 +5,7 @@ class Twitter extends Component {
     render() {
         return (
           <a
-            className="twitter-share-button clearfix"
+            className="twitter-share-button"
             href={`https://twitter.com/intent/tweet?text=made+this+meme+with+Meme+in+a+Giffy&hashtags=memeinaGiffy&url=http://localhost:3001/display/${this.props.memeId}`}
             data-size="large"
           >
