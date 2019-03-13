a0a99ff17c932ebef36acbf645bb782aed0cea38
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-03-12 21:58:12 -0400

    last last one :boom:

--- src/components/twitter/Twitter.js
+++ src/components/twitter/Twitter.js
@@ -8,10 +8,8 @@ class Twitter extends Component {
             className="twitter-share-button"
             href={`https://twitter.com/intent/tweet?text=made+this+meme+with+Meme+in+a+Giffy&hashtags=memeinaGiffy&url=http://localhost:3001/display/${this.props.memeId}`}
             data-size="large"
-          >
-            <button type='button' className='twitter-share-button'>         
-              <img src={TwitterLogo} alt="Twitter logo" className="twitter-share-image"/>
-            </button>
+          >   
+           <img src={TwitterLogo} alt="Twitter logo" className="twitter-share-image"/>
           </a>
         );
     }
